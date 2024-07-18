27a4bde9fe7da8ab7d5a91b9db969e83102241c0
Joana-Rosinha-Neves <joanarosinhan@gmail.com>
2024-07-18 16:27:41 +0100

    SDSD
    DSDS

--- pastaalgoritmos/LancamentoDados-copy/DadosAula3.java
+++ pastaalgoritmos/LancamentoDados-copy/DadosAula3.java
@@ -9,6 +9,7 @@ class DadosAula3
     //variaveis globais de clase. talbem denominadas de propeidades ou atributos
     // para computador criar progrma que permite pessoas escreverem
     Scanner scanNumeroUtilizador= new Scanner(System.in);
+    Random objetoNumeroAletatorio= new Random ();
     
     
     
@@ -16,19 +17,56 @@ class DadosAula3
     DadosAula3()
     {
       pedidoLancamento();  
-      
+     
     }
     
     void pedidoLancamento()
     {
-        System.out.println("Bem-vindo/a ao Simulador de lançamentos");
-        System.out.println("Este programa permite lançar dados de 4 dados e de 6 lados");
-        System.out.println("Por favor escrever 4 ou 6 para determinar genero de dado a lançar");
-        // para ter o dado que pessoas escrevem.
+       System.out.println("Bem-vindo/a ao Simulador de lançamentos");
+          System.out.println("Este programa permite lançar dados de 4 dados ou 6 ou 8 lados");
+       
+        
+        // guardar valor que o utlizador escreveu
         int  valorUtilizadorGuardado = scanNumeroUtilizador.nextInt();
         
         
+        if( valorUtilizadorGuardado == 4 || valorUtilizadorGuardado==6 || valorUtilizadorGuardado==8)
+        {
+            escreveResultado(valorUtilizadorGuardado); // salta para função o parametro da funçao-
+        }
+        else
+        { 
+            pedidoLancamento2();
+        } 
+            
     
+     
     }
     
-}
+    void pedidoLancamento2()
+    {
+            System.out.println("Não escolheu um genero de dados suportado");
+            System.out.println("Por favor escrever 4 ou 6 ou 8 para determinar genero de dado a lançar");
+            int  valorUtilizadorGuardado = scanNumeroUtilizador.nextInt();
+            if ( valorUtilizadorGuardado == 4 || valorUtilizadorGuardado==6 || valorUtilizadorGuardado==8)
+            {
+             escreveResultado(valorUtilizadorGuardado);
+            }
+            else 
+             {
+             pedidoLancamento2();
+            }
+            }
+    
+    
+    void escreveResultado(int generoDeDadoAlancar)//o nome diferente
+    {
+        int resultadoDoLancamento= objetoNumeroAletatorio.nextInt(generoDeDadoAlancar)+1;
+        
+        System.out.println("O resultado do lançamento foi: " + resultadoDoLancamento);
+        
+    
+        }        
+    
+ }
+    
