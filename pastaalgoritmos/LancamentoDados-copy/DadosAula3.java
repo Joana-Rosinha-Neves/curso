d10ebf34d28f789fb25f0e87415d254385946d53
Joana-Rosinha-Neves <joanarosinhan@gmail.com>
2024-07-18 10:22:11 +0100

    wewe
    ewewe

---
+++ pastaalgoritmos/LancamentoDados-copy/DadosAula3.java
@@ -0,0 +1,20 @@
+//importação de class que nos permite gerar numero aleatoria
+import java.util.Random;
+
+
+class DadosAula3
+{
+    DadosAula3()
+    {
+      pedidoLancamento();  
+    }
+    
+    void pedidoLancamento()
+    {
+        System.out.println("Bem-vindo/a ao Simulador de lançamentos");
+        System.out.println("Este programa permite lançar dados de 4 dados e de 6 lados");
+        System.out.println("Por favor escrever 4 ou 6 para determinar genero de dado a lançar");
+        
+    
+    }
+}
