2a7c9686ed2b4e21dfe540f0a07f91eacddcf61b
Joana-Rosinha-Neves <joanarosinhan@gmail.com>
2024-07-19 16:24:14 +0100

    ee
    ewewe

---
+++ pastaalgoritmos/Operacoescalculos/TotalMinutos.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class TotalMinutos
+
+{
+    TotalMinutos()
+    {
+        System.out.println (" insira o numero de dias, sendo este numero inteiro e positivo");
+        
+        Scanner ScanNumeroDito =new Scanner(System.in);
+        int valordado= ScanNumeroDito.nextInt();
+    
+        
+        int passarMinutos= valordado*1440;
+        System.out.println (" o valor final"+ passarMinutos);
+        
+        
+    }
+    
+    
+     
+     
+     
+    
+     
+     
+}
+    
+    
