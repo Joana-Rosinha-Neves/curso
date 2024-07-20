951a33e24cda85ccaa5d74fb79d1ab1916548d30
joana <joanarosinhan@gmail.com>
2024-07-20 15:00:42 +0100

    dsd
    dsdsd

--- pastaalgoritmos/Operacoescalculos-copy/Username.java
+++ pastaalgoritmos/Operacoescalculos-copy/Username.java
@@ -2,18 +2,59 @@ import java.util.Scanner;
 // Pedir ao utilizador que escreva a palavra para username e em seguida a palavra para a 
 
 
-
  
 class Username
 {
+    
+Scanner scanTexto = new Scanner(System.in);
+Scanner scanPassword= new Scanner(System.in);
+int contador = 0;
+
+
         Username ()
         {
-        Scanner scantexto = new Scanner(System.in);
-        String escritaUtilizador= new String();
-        System.out.println ( "Qual o seu usernmane");
-        escritaUtilizador = scantexto.nextLine();
-        System.out.println ( "Qual a sua password");
-        password= scantexto.nextLine();
-        
-            if ((escritaUtilizador.equalsIgnoreCase("norte") == true) ||  password.equalsIgnoreCase (“abz”) ==true) )
+         
+        inicio();
+            
+
         }
+        
+        
+                
+        
+    
+        void inicio()
+        
+        {
+           if  (contador == 3)
+        {
+            System.out.println ("Excedeu as 3 vezes");
+            return ;
+        } 
+        else
+        {
+        
+        System.out.println( "Qual o seu usernmane");
+        String utilizador = scanTexto.nextLine();
+        System.out.println ( "Qual a sua password");
+        String password = scanPassword.nextLine();
+    
+            if (utilizador.equalsIgnoreCase("utilizador") == true && password.equalsIgnoreCase("abz")== true) 
+        {
+            System.out.println("Autentificação completa");
+        }
+        else 
+        {
+            System.out.println ("Autentificação inválida");
+            contador++;
+            inicio();
+        }
+            
+  
+    }
+
+}
+}
+   
+    
+   
