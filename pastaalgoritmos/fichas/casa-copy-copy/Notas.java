14cd260447f2830a9a5b2d6f5179dec878d28800
joana <joanarosinhan@gmail.com>
2024-07-24 01:18:16 +0100

    sdsd
    dsdsd

---
+++ pastaalgoritmos/fichas/casa-copy-copy/Notas.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+class Notas 
+{
+    Scanner notascanner = new Scanner(System.in);
+    int contador = 0;
+    
+    
+    Notas()
+    {
+        
+        
+        mediadasnotas();
+        
+         
+    }
+    void mediadasnotas()
+    {
+    System.out.println ( " Por favor insira nota " );       
+       int notaUtilizador2 = notascanner.nextInt();
+        contador++;
+        notauTotal=notaUtilizador2 + notaUtilizador3
+        if (contador == 3)
+        {
+        
+        int media = notafinal / 3;
+       System.out.println ("o valor da media é" +  media);
+    }
+           
+    }
+        
+}  
