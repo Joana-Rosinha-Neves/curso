71247a1f00a2f23276f376f253db40cb54c52621
joana <joanarosinhan@gmail.com>
2024-09-16 14:55:27 +0100

    sdsd
    cxcxc

--- web/web-server-side/aula1609/js/main.js
+++ web/web-server-side/aula1609/js/main.js
@@ -1,178 +1,55 @@
 
 
-// let product={
-// nome:"joana",
-// inStock: true,
-// price:1.99,
-// colors: ["vermelho", "azul", "verde" ],
-
-// }
-// console.log(product.price)
-// console.log(produt.colors[2])
-// product.price=2.55;
-
-
-// // ------------------------- EXERCÍCIOS AULA2 ----------------------------
-
-// //EXER 1 
-// const WORD = "skateboard";
-
-// let facialHair = WORD.slice(5, 10); // Cortar da posição 5 à 10, vai ser guardado board na variável
-// facialHair = facialHair.replace("o", "e"); // Substituir letras na palavra
-// console.log(facialHair);
 
 
 
 
-// //EXER 2 
-// let diaSemana = prompt("Qual o dia da semana?").toLowerCase(); // Colocar em minusculas
-// switch(diaSemana){
-//     case "sexta":
-//     alert("Yay sobrevivemos a mais uma semana!");
-//     break;
 
-//     default:
-//     alert("Não é sexta!");
-// }
 
-// let pw = prompt("Insira uma password com 6 caracteres no minimo");
-// if(pw != null && pw.length > 5){
-//     alert("Success!!")
-// }
-// else{
-//     alert("Incorrect Password!")
+
+// //  Guarde o seguinte array no seu código:
+//  const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, 
+//     {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 
+//     'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape’}]
+
+;
+//  2. Crie um novo array chamado firstNames que nos retorne os primeiros Nomes do 
+// Array Original
+
+// const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
+
+// fullNames.forEach(function (fullNames) {
+//     console.log(fullNames.first)
+// })
+
+// const primeirosnomes = fullNames.map(function (t) {
+//     return t.first;
+
+// // // })
+// console.log(primeirosnomes)
+// // Usando as Arrow Functions, crie uma função chamada greet que receba um 
+// // argumento que represente o nome de uma pessoa
+
+// const greet = (x) => {
+//     return "ola" + x;
 // }
 
+// console.log(greet("joana"))
 
-
-// //EXER 3
-// let planetas = [ "Mercúrio", "Vénus", "Terra", "Mart", "Jupiter", "Saturno", "Urano", "Neptuno", "Plutão"];
-// planetas[3] = "Marte"; // Alterar posição específica no Array
-// planetas.pop(); // Retirar elemento do fim do Array
-// planetas.unshift("New"); // Acrescentar elemento ao início do Array
-// console.log(planetas);
-
-
-
-// //EXER 4
-// const airplaneSeats = [
-//     ['Ruth', 'Anthony', 'Stevie'],
-//     ['Amelia', 'Pedro', 'Maya'],
-//     ['Xavier', 'Ananya', 'Luis'],
-//     ['Luke', null, 'Deniz'],
-//     ['Rin', 'Sakura', 'Francisco']
-// ];
-
-// airplaneSeats[3][1] = "Joaquim";
-// console.log(airplaneSeats);
-
-
-
-
-// //EXER 5
-// let product = {
-//     nome: "PC",
-//     inStock: true,
-//     Price: 1.99,
-//     Colors: ["vermelho", "azul", "verde"],
+// Escreva uma função chamada validUserNames que aceite um array de 
+// usernames (que serão strings). 
+// 2. A nossa função deverá retornar um novo array contendo apenas os usernames
+//  que têm menos de 10 caracteres
+// function validusernames(list) {
+//     const usernames = list.filter(element => element.length < 10);
+//     console.log(usernames)
 // }
 
-// console.log(product.Price);
-// console.log(product.Colors[2]);
-// product.Price = 2.55;
-// // ---------------------------------------------------------------------------------------------------------------------------------------
+// Escreva uma função chamada allEvens que aceite um array de números. 
+// 2. A nossa função deverá retornar verdadeiro se todos os números forem pares
 
-
-
-
-
-// // -------------------------- MATÉRIA AULA2 -------------------------------
-// // PROMPT E TYPEOF
-// let myName = prompt("Qual o seu nome?");
-// console.log(typeof(myName)); // Função para descobrir o tipo de variável
-
-
-// // ARRAYS
-// let series = ["Lost", "Friends", "Dark", "Breaking Bad", "House of Dragons", "The Bear", "Station 11"];
-// series[1] = "DragonBall"
-
-// series.push("GOT"); //Adicionar ao fim do array uma série
-// series.unshift("Halt and Catch Fire"); //Adicionar ao início do array uma série
-
-// console.log(series);
-
-
-// //ARRAYS BIDIMENSIONAIS
-// let moreCourses = [
-//     ["Apis", "DOM", ["JS", "C"]],
-//     ["CSS", "HTML"],
-//     "Algoritmia",
-//     true,
-//     16,
-//     "Android"
-// ];
-
-// //Aceder à posição do ArrayBidiomensional courses[1][1] = HTML
-
-// //OBJECT LITERALS
-// let JS = {
-//     avaliação : ["Teste", "Exame"],
-//     formador : "Sara monteiro",
-//     sala : 6,
-//     horas : 50,
-//     ferias : true,
+// function allEvans(list) {
+//     return list.every(w => {
+//         w % 2 == 0
+//     })
 // }
-
-// console.log("A avaliação é " + JS.avaliação);
-// console.log("A formadora é " + JS.formador);
-// // // -------------------------------------------------
-// //exercicio 
-// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "fred"];
-
-// for (let i=0; i<people.length;i++)
-
-    
-// {
-//     console.log(people[i].toUpperCase());
-// }
-
-// 1. Lembra-se da música I'm blue dos Eiffel 65? Vamos usar o ciclo for para 
-// escrever 6 vezes o '"Da ba dee da ba daa’;
-
-// let musica = "Da ba dee da ba daa";
-// for (let i= 1; i<=6; i++)
-// {
-//     console.log(musica);
-// }
-// Faça um ciclo for que imprima estes números, por esta ordem:
-// •25
-// •20
-// •15
-// •10
-// •5
-
-// for (let i=25; i>=5;i-=5)
-// {
-//     console.log(i);
-// }
-
-// Utilizando os loops e as condições que já aprendemos, construa uma lista de 
-// compras.
-//  Dicas:
-//  • A lista é um array.
-//  • Fazer prompt para o utilizador adicionar items na lista, e adicionar esses items
-//  ao array.
-//  • Criar uma variável "código" e se o utilizador digitar isso, o ciclo acaba e a lista 
-// aparece. Por exemplo, se digitar 'fim', deixam de aparecer prompts.
-
-// //
-// lista = []
-// let codigo = "a";
-
-// while (codigo!= "fim") 
-// {
-//     codigo = prompt("lista")
-//     lista.push(codigo); 
-// }
-// console.log(lista );
-kk
---
+++ web/web-server-side/exerciciobotao - Cópia/js/main.js
@@ -0,0 +1,10 @@
+
+
+function Rant(message){
+for (let i=0; i<3;i++)
+{
+    console.log( message.toUpperCase());
+}
+
+}
+
