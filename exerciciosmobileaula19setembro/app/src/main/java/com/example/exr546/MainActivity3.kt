d3a74c3e00a5e25264ce66ae66b7b4e3cdcd32f1
joana <joanarosinhan@gmail.com>
2024-09-24 22:13:38 +0100

    vc
    cvcv

--- exerciciosmobileaula19setembro/app/src/main/java/com/example/exr546/MainActivity3.kt
+++ exerciciosmobileaula19setembro/app/src/main/java/com/example/exr546/MainActivity3.kt
@@ -1,20 +1,38 @@
 package com.example.exr546
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.exr546.databinding.ActivityMain3Binding
+import com.example.exr546.databinding.ActivityMainBinding
 
 class MainActivity3 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main3)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn_valid)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    private val binding by lazy {
+        ActivityMain3Binding.inflate(layoutInflater)
     }
-}
+
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+
+                setContentView(binding.root)
+
+
+
+                Toast.makeText(applicationContext, "login errado ", Toast.LENGTH_SHORT).show()
+
+                binding.btnLoginerrado.setOnClickListener {
+
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                }
+            }
+        }
+
+
