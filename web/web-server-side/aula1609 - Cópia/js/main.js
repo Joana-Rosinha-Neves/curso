008f0b852f4eedea80d96f9065f94e2d068df644
joana <joanarosinhan@gmail.com>
2024-09-26 14:07:48 +0100

    fgfg
    fgfgfg

---
+++ web/web-server-side/aula1609 - Cópia/js/main.js
@@ -0,0 +1,37 @@
+
+
+// let text = "A turma de web é dinamica e tem as caracteristicas essenciais para brilhar numa empresa"
+
+// let posicao = text.indexOf("u");
+// let posicao2 = text.indexOf("w");
+// let sizestex = text.length
+
+// console.log( " a letra w aparece pela primeira vez na posicao" + posicao)
+// console.log( " a letra u aparece pela primeira vez na posicao" + posicao2)
+// console.log(" a string tem" + sizestex)
+
+//2
+
+let prendas = []
+
+
+
+for (let i=0; i<length;i++)
+{
+    let prenda = prompt( "prenda")
+    prendas.push(prenda[i]);
+
+}
+console.log(" a lista tem" + prendas)
+
+
+
+
+
+
+
+
+
+
+
+
